e88ec9e017dd562634ac1eb4753e1fc0a187b358
modegcj <2465135939@qq.com>
2018-11-05 14:53:12 +0800

    新增项目

---
+++ nodeproject/express_demo.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+// 主页输出"Hello World"
+app.get('/',function(req,res){
+    res.send('Hello World');
+})
+
+// POST请求
+app.post('/',function(req,res){
+    console.log("主页POST请求");
+    res.send('Hello POST');
+})
+
+
+var server = app.listen(8081,function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("应用实例，访问地址为http://%s:%s",host,port)
+})
