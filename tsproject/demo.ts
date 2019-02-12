d2897193c4587534a7b436e9579d2528e210eb18
modegcj <2465135939@qq.com>
2019-02-12 09:55:38 +0800

    ‘‘node网站’

---
+++ tsproject/demo.ts
@@ -0,0 +1,11 @@
+function error(message: string): never{
+    throw new Error(message);
+}
+function fail(){
+    return error("Something failed");
+}
+function infiniteLoop(): never{
+    while (true){
+        
+    }
+}
